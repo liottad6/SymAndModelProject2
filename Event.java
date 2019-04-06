54efb2b8d87d9a3693c944272057b6cb2b19eda8
jpk1101 <jpk.mymail@gmail.com>
2019-04-06 19:45:31 -0400

    Work in progress for Event
    The system itself seems to be working okay.
    Change the String mode to "QueueList" in DisplayQueue() to debug and see how the queue lists are working.
    The numerical results from Samst() and OutSampst() are still coming out off. Not sure yet why.

--- Event.java
+++ Event.java
@@ -8,7 +8,7 @@
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.ListIterator; 
+import java.util.ListIterator;
 
 public class Event
 {
@@ -25,6 +25,16 @@ public class Event
     private int                         eventType;
     private int                         teller;
 
+    static private double min = 999999;             //min queue length
+    static private double max;             //max queue length
+    static private double nObservations;   //total num of delays
+    static private double sum;             //total delays
+
+    private double [] areaTimest;
+    private double [] tlvcTimest;
+    private double [] prevalTimest;
+    private double [] minTimest;
+    private double [] maxTimest;
     /**
      * Constructor for objects of class Event
      */
@@ -55,8 +65,31 @@ public class Event
         this.teller       = teller;
     }
 
-    /**
-     */
+    //uses max, min, sum, sum, nObservations lists, increment when proper argument is called
+    static public void Sampst(double val, int sampstvar){
+        if (val < min)
+            min = val;
+        if (val > max)
+            max = val;
+        nObservations ++;
+        sum += val;
+    }
+
+    //
+    static public void OutSampst(int lowvar, int highvar){
+        System.out.println("sampst var number: " + Integer.toString(1));
+        System.out.println("avg: " + Double.toString(sum/nObservations));
+        System.out.println("num of vals: " + Double.toString(nObservations));
+        System.out.println("max: " + max);
+        System.out.println("min: " + min);
+    }
+
+    static public double Filest(int qNum){
+        return 0;
+    }
+    //double result = Timest(0.0, -(TIM_VAR + qNum));
+    //return result;
+
     public double GetEventTime()
     {
         return eventSimTime;
@@ -64,6 +97,9 @@ public class Event
 
     /**
      */
+    static double Timest(double value, int qNum){
+        return 0.0;
+    }
     public int GetEventType()
     {
         return eventType;
@@ -76,7 +112,18 @@ public class Event
 
     static public void EventSchedule(Event ev)
     {
-        eventList.add(ev);
+        int current_index = 0;
+        boolean added = false;
+        while (current_index < eventList.size()) {
+            if (ev.eventSimTime < eventList.get(current_index).eventSimTime) {
+                eventList.add(current_index, ev);
+                added = true;
+                break;
+            }
+            current_index ++;
+        }
+        if (current_index == eventList.size() & !added)
+            eventList.addLast(ev);
     }
 
     static public void InsertInQueue(Event ev, Order order, int qNum)
@@ -86,23 +133,31 @@ public class Event
         } else if (order == Order.LAST){
             queueLists.get(qNum).addLast(ev);
         } else if (order == Order.INCREASING){
+            int iter = 0;
+            while (iter < queueLists.get(qNum).size()) {
 
-        } else { //decreasing
+                iter ++;
+            }
+        } else { //decreasing, unused
 
         }
     }
 
     static Object RemoveFromQueue(Order order, int qNum)
     {
+        if (qNum == MAX_QUEUES+1){
+            return eventList.remove(0);
+        }
         if(order == Order.FIRST)
         {
-            queueLists.get(qNum).removeFirst();
+            //queueLists.get(qNum).removeFirst();
+            return queueLists.get(qNum).remove(0);
         }
         else //last
-            {
-            queueLists.get(qNum).removeLast();
-            }
-        return null;
+        {
+        //queueLists.get(qNum).removeLast();
+            return queueLists.get(qNum).remove(queueLists.get(qNum).size()-1);
+        }
     }
 
 
@@ -110,13 +165,17 @@ public class Event
     {
         eventList = new LinkedList<>();
         queueLists = new ArrayList<>();
-        int iter = 0;
-        while (iter <= MAX_QUEUES) {
+        int iter = 1;
+        while (iter <= MAX_QUEUES+1) {
             LinkedList<Event> list = new LinkedList<>();
             queueLists.add(list);
             iter ++;
         }
         simTime = 0;
+        min = 0;
+        max = 0;
+        nObservations = 0;
+        sum = 0;
     }
 
     static public double GetSimTime()
@@ -136,25 +195,69 @@ public class Event
 
     static public int GetQueueSize(int qNum)
     {
-        int sum = 0;
-        int iter = 0;
-        while (iter < qNum) {
-            if (!queueLists.get(iter).isEmpty())
-                sum += queueLists.get(iter).size();
-            iter ++;
+        if (qNum == MAX_QUEUES+1){
+            return eventList.size();
+
         }
-        return sum;
+        else
+            return (queueLists.get(qNum).size());
     }
 
     static public void Timing()
     {
-        Event ev      = (Event)RemoveFromQueue(Order.FIRST, 25);
-        simTime       = ev.GetEventTime();
-        nextEventType = ev.GetEventType();
-        tellerNumber  = ev.GetTeller();
+        if (eventList.size() > 0) {
+            Event ev = (Event) RemoveFromQueue(Order.FIRST, 25);
+            simTime = ev.GetEventTime();
+            nextEventType = ev.GetEventType();
+            tellerNumber = ev.GetTeller();
+        }
+
         
     }
-    static public void EventCancel(int event){
-        eventList.remove(event);
+    static public void EventCancel(int eventType){
+        //iterates eventlist and removes the first event with this type
+        int iter = 0;
+        while (iter < eventList.size()){
+            if (eventList.get(iter).eventType == eventType){
+                eventList.remove(iter);
+                return;
+            }
+            iter ++;
+        }
+    }
+    static public void DisplayQueue(){
+        String mode = "x";
+        if (mode.equals("EventList")) {
+            int iter = 0;
+            System.out.println("###DISPLAY_QUEUE###");
+            while (iter < eventList.size()) {
+                System.out.println(eventList.get(iter).eventType);
+                iter++;
+            }
+            System.out.println("###END_DISPLAY###");
+        } else if (mode.equals("QueueList")) {
+            System.out.println("###DISPLAY_QUEUE###");
+            System.out.println("SIM TIME: " + simTime);
+            System.out.println("0:" + queueLists.get(0).toString());
+            System.out.println("1:" + queueLists.get(1).toString());
+            System.out.println("2:" + queueLists.get(2).toString());
+            System.out.println("3:" + queueLists.get(3).toString());
+            System.out.println("4:" + queueLists.get(4).toString());
+            System.out.println("5:" + queueLists.get(5).toString());
+            System.out.println("6:" + queueLists.get(6).toString());
+            System.out.println("7:" + queueLists.get(7).toString());
+            System.out.println("8:" + queueLists.get(8).toString());
+            System.out.println("9:" + queueLists.get(9).toString());
+            System.out.println("10:" + queueLists.get(10).toString());
+            System.out.println("11:" + queueLists.get(11).toString());
+            System.out.println("12:" + queueLists.get(12).toString());
+            System.out.println("13:" + queueLists.get(13).toString());
+            System.out.println("14:" + queueLists.get(14).toString());
+            System.out.println("15:" + queueLists.get(15).toString());
+            System.out.println("###END_DISPLAY###");
+        }
+    }
+    public String toString(){
+        return "<" + eventSimTime + ">";
     }
 }
